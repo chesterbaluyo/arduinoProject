309eaac1b4550246f9f363109f65fd385e0ce80c
Chester Troy Baluyo <chester.baluyo@aestrea.com>
2013-06-15 17:17:41 +0800

    White space changes

--- FingerPrintGSMController/FingerPrintGSMController.ino
+++ FingerPrintGSMController/FingerPrintGSMController.ino
@@ -50,7 +50,6 @@ void setup() {
 }
 
 void loop() {
-
         if(digitalRead(engineSwitch)) {
                 //TODO Test if condition should runs once          
                 Serial.println("Should see this once.");
@@ -105,152 +104,152 @@ void initializePin() {
 }
 
 void initializeGSM() {
-          Serial.print("Initialize GSM: ");  
-          sendATCommand("AT");
-          receiveGSMResponse();
-          clearGsmResponseMessage();                    
+        Serial.print("Initialize GSM: ");  
+        sendATCommand("AT");
+        receiveGSMResponse();
+        clearGsmResponseMessage();                    
 }
 
 void setSMSMessageFormat() {
-          Serial.print("Set SMS message format: ");  
-          sendATCommand("AT+CMGF=1");
-          receiveGSMResponse();
-          clearGsmResponseMessage();
+        Serial.print("Set SMS message format: ");  
+        sendATCommand("AT+CMGF=1");
+        receiveGSMResponse();
+        clearGsmResponseMessage();
 }
 
 void enableAutomaticAnswer() {
-          Serial.print("Enable automatic answering: ");  
-          sendATCommand("ATS0=1");
-          receiveGSMResponse();
-          clearGsmResponseMessage();
+        Serial.print("Enable automatic answering: ");  
+        sendATCommand("ATS0=1");
+        receiveGSMResponse();
+        clearGsmResponseMessage();
 }
 
 void enableLoudSpeaker() {
-          Serial.print("Loud speaker mode: ");   
-          sendATCommand("ATM9"); 
-          receiveGSMResponse();
-          clearGsmResponseMessage();
+        Serial.print("Loud speaker mode: ");   
+        sendATCommand("ATM9"); 
+        receiveGSMResponse();
+        clearGsmResponseMessage();
 }
 
 void setLoudnessToMax() {
-          Serial.print("Set maximum loudness: ");   
-          sendATCommand("ATL9");
-          receiveGSMResponse();
-          clearGsmResponseMessage();          
+        Serial.print("Set maximum loudness: ");   
+        sendATCommand("ATL9");
+        receiveGSMResponse();
+        clearGsmResponseMessage();          
 }
 
 void gsmSMSListener() {
-          if(receiveGSMResponse()) {
-                if(gsmResponseMessage.substring(2,6)=="+CMT"){
-                        readSMSCommand();                        
-                        smsCount++;
-                        if(smsCount >= 15) {
-                                deleteAllSMS();
-                                smsCount = 0;
-                        }
-                }
-                clearGsmResponseMessage();    
-          }      
+        if(receiveGSMResponse()) {
+              if(gsmResponseMessage.substring(2,6)=="+CMT"){
+                      readSMSCommand();                        
+                      smsCount++;
+                      if(smsCount >= 15) {
+                              deleteAllSMS();
+                              smsCount = 0;
+                      }
+              }
+              clearGsmResponseMessage();    
+        }      
 }
 
 void sendATCommand(String atCommand) {          
-          gsm.println(atCommand);
+        gsm.println(atCommand);
 }
 
 boolean receiveGSMResponse() {
-          boolean isAvailable = false;
-          
-          while(gsm.available()>0) {
-                  char incomingData = '\0';
-                  incomingData = gsm.read(); 
-                  gsmResponseMessage += incomingData;
-                  isAvailable = true;
-          }
+        boolean isAvailable = false;
+        
+        while(gsm.available()>0) {
+                char incomingData = '\0';
+                incomingData = gsm.read(); 
+                gsmResponseMessage += incomingData;
+                isAvailable = true;
+        }
  
-          if(isAvailable) {
-                  Serial.println(gsmResponseMessage);          
-          }
-          else {
-                  Serial.println("*** GSM connection failed! ***");
-          } 
-          return isAvailable;
+        if(isAvailable) {
+                Serial.println(gsmResponseMessage);          
+        }
+        else {
+                Serial.println("*** GSM connection failed! ***");
+        } 
+        return isAvailable;
 }
 
 void clearGsmResponseMessage() {
-          gsmResponseMessage = String("");
+        gsmResponseMessage = String("");
 }
 
 void deleteAllSMS() {
-          for(int i=1;i<=15;i++) {
-                  String atCommand = "AT+CMGD=";
-                  atCommand += i;
-                  sendATCommand(atCommand);
-                  delay(1000);
-          }
+        for(int i=1;i<=15;i++) {
+                String atCommand = "AT+CMGD=";
+                atCommand += i;
+                sendATCommand(atCommand);
+                delay(1000);
+        }
 }
 
 void readSMSCommand() {
-          int smsIndexLocation = 0;
-          String command;
-          String password;
-          String sms;
-          
-          smsIndexLocation = gsmResponseMessage.indexOf("\"");
-          sms = gsmResponseMessage.substring(smsIndexLocation+1);
-          while(sms.indexOf("\"")>=0) {
-                  smsIndexLocation = sms.indexOf("\"");
-                  sms = sms.substring(smsIndexLocation+1);
-          }
-          
-          sms = sms.substring(2);
-          smsIndexLocation = sms.indexOf(32);
-          command = sms.substring(0,smsIndexLocation);
-          password = sms.substring(smsIndexLocation+1,smsIndexLocation+7);       
-          
-          if(command == "STOP" && password == userPassword) {
-                  digitalWrite(starterRelay, LOW);
-                  sendSMSAlert("Engine is stop.");          
-          }
-          if(command == "OVERRIDE" && password == userPassword) {
-                  digitalWrite(starterRelay, HIGH);
-                  starterRelayIsOff = false;
-                  sendSMSAlert("Engine Start.");                  
-          }
-          if(command == "RENEW" && password == userPassword) {
-                  digitalWrite(starterRelay, LOW);           
-                  deleteIsActive = true;                  
-          }        
+        int smsIndexLocation = 0;
+        String command;
+        String password;
+        String sms;
+        
+        smsIndexLocation = gsmResponseMessage.indexOf("\"");
+        sms = gsmResponseMessage.substring(smsIndexLocation+1);
+        while(sms.indexOf("\"")>=0) {
+                smsIndexLocation = sms.indexOf("\"");
+                sms = sms.substring(smsIndexLocation+1);
+        }
+        
+        sms = sms.substring(2);
+        smsIndexLocation = sms.indexOf(32);
+        command = sms.substring(0,smsIndexLocation);
+        password = sms.substring(smsIndexLocation+1,smsIndexLocation+7);       
+        
+        if(command == "STOP" && password == userPassword) {
+                digitalWrite(starterRelay, LOW);
+                sendSMSAlert("Engine is stop.");          
+        }
+        if(command == "OVERRIDE" && password == userPassword) {
+                digitalWrite(starterRelay, HIGH);
+                starterRelayIsOff = false;
+                sendSMSAlert("Engine Start.");                  
+        }
+        if(command == "RENEW" && password == userPassword) {
+                digitalWrite(starterRelay, LOW);           
+                deleteIsActive = true;                  
+        }        
 }
 
 void sendSMSAlert(String message) {
-          char ctl_z = 0x1A;
-          String atCommand = "AT+CMGS= \"";
-          
-          atCommand += userNumber + "\"";
-          sendATCommand(atCommand);
-          delay(300);
-          
-          atCommand = message + ctl_z;
-          sendATCommand(atCommand);
-          delay(90);
+        char ctl_z = 0x1A;
+        String atCommand = "AT+CMGS= \"";
+        
+        atCommand += userNumber + "\"";
+        sendATCommand(atCommand);
+        delay(300);
+        
+        atCommand = message + ctl_z;
+        sendATCommand(atCommand);
+        delay(90);
 }
 
 boolean checkFingerPrint() {
-      receiveResponsePacket();  
-      if((fpShieldResponsePacket[9] == 1)) { 
-              starterRelayIsOff = false;
-              clearPacket(fpShieldResponsePacket);
-              return true;              
-      }        
-      Serial.println("FAILED. Please try again.");
-      clearPacket(fpShieldResponsePacket);
-      return false;
-}
-
-void clearPacket(byte *packet) {
-	for(int i=0; i<=50; i++) {
-		packet[i] = 0x00;	
-	}
+        receiveResponsePacket();  
+        if((fpShieldResponsePacket[9] == 1)) { 
+                starterRelayIsOff = false;
+                clearPacket(fpShieldResponsePacket);
+                return true;              
+        }        
+        Serial.println("FAILED. Please try again.");
+        clearPacket(fpShieldResponsePacket);
+        return false;
+  }
+  
+  void clearPacket(byte *packet) {
+  	for(int i=0; i<=50; i++) {
+  		packet[i] = 0x00;	
+  	}
 }
 
 void scanFingerPrint() {
@@ -322,6 +321,7 @@ void enrollFingerPrint() {
 }
 
 void readDTMF() {
+        Serial.print("DTMF: ");
         char thisChar;
         dtmf.sample(dtmfSensor);
         dtmf.detect(dMags, 506);
